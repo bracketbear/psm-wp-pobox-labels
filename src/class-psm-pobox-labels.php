e50fa0cadfc38c8ca7f6e7fef2aa93cceda8fe77
Harrison Callahan <harrison@harrisoncallahan.com>
2019-06-25 13:42:04 -0700

    Changed filter used.

--- src/class-psm-pobox-labels.php
+++ src/class-psm-pobox-labels.php
@@ -6,15 +6,13 @@ class PSM_POBox_Labels
 {
   public function init() 
   {
-    add_filter('woocommerce_checkout_fields', [$this, 'overrideFields']);
+    add_filter('woocommerce_default_address_fields', [$this, 'overrideFields']);
   }
 
   public function overrideFields($fields)
   {
-    $fields['billing']['billing_address_1']['label'] = 'Address 1 (no PO Boxes allowed)';
-    $fields['billing']['billing_address_2']['label'] = 'Address 2 (no PO Boxes allowed)';
-    $fields['shipping']['shipping_address_1']['label'] = 'Address 1 (no PO Boxes allowed)';
-    $fields['shipping']['shipping_address_2']['label'] = 'Address 2 (no PO Boxes allowed)';
+    $fields['address_1']['label'] = 'Address 1 (no PO Boxes allowed)';
+    $fields['address_2']['label'] = 'Address 2 (no PO Boxes allowed)';
 
     return $fields;
   }
